42830b15e43930869926feaf7318e8090e7a3526
Esraberra <esraberra6@gmail.com>
2021-10-15 22:40:11 +0200

    day11 all classes

---
+++ src/test/java/tests/day11/C03_FileExist.java
@@ -0,0 +1,37 @@
+package tests.day11;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C03_FileExist  {
+
+
+
+@Test
+    public void test(){
+    System.out.println(System.getProperty("user.home"));
+
+    //   /Users/esrakucuk/Downloads/picture.jpg
+
+    String dosyaYolu=System.getProperty("user.home") + "/Downloads/picture.jpg";
+
+    //System.out.println(Files.exists(Paths.get(dosyaYolu))); birinci yol
+Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+
+    System.out.println(System.getProperty("user.dir"));
+
+}
+
+
+
+
+
+
+
+
+}
